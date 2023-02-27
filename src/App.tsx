2a64989607a0581172732699583c06c73d6d912e
azerose <woojjang8671@naver.com>
2023-02-27 16:10:37 +0900

    FEATURE: 초기설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainWrapper>
+      <div>환전 계산기</div>
+      <FormWrapper>
+        <ChangeWrapper>
+          <div>
+            <input />
+            <select></select>
+          </div>
+          <button>변환하기</button>
+        </ChangeWrapper>
+        <input readOnly />
+      </FormWrapper>
+    </MainWrapper>
   );
 }
+
+const MainWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const FormWrapper = styled.form`
+  display: flex;
+  flex-direction: column;
+  margin-top: 20px;
+  max-width: 500px;
+  min-width: 400px;
+  width: 50%;
+  input {
+    height: 20px;
+  }
+`;
+
+const ChangeWrapper = styled.div`
+  display: flex;
+  width: 100%;
+  input {
+    width: 80%;
+  }
+  button {
+    width: 20%;
+  }
+  margin-bottom: 10px;
+`;
 
 export default App;
